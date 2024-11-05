10f829dd7d832d46c75dd6400100e07c182991b1
theoxav <theoxxav@outlook.fr>
2024-11-05 15:51:49 +0100

    Config environment file and add TMDB api

---
+++ src/services/movies.js
@@ -0,0 +1,14 @@
+import 'server-only';
+
+export const getMovieByPath = async (path, language = 'fr-FR') => {
+  const url = new URL(`${process.env.TMDB_API_URL}${path}`);
+  url.searchParams.append('api_key', process.env.TMDB_API_KEY);
+  url.searchParams.append('language', language);
+
+  try {
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
