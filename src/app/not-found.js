f1a8a958b01dba8dd79f3bb5266b1b1d791093ac
theoxav <theoxxav@outlook.fr>
2024-11-05 15:01:30 +0100

    Add error and not-found pages

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups, la page que vous avez demandez n&apos;existe pas</p>
+    </div>
+  );
+};
+
+export default NotFound;
